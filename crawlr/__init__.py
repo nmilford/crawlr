c789f484fd152d36ae4eca63ab70ff211bfbab39
Nathan Milford <nathan@milford.io>
2015-12-12 15:46:46 -0500

    added error handlng

--- crawlr/__init__.py
+++ crawlr/__init__.py
@@ -21,9 +21,11 @@ class Crawlr:
   """
 
   def __init__(self, url):
-    self.crawl_data = {}
     self.url = urlnorm.norm(url)
     self.tld = get_tld(url).encode('ascii','ignore')
+
+    self.crawl_data = {}
+    self.crawl_data['failure'] = False
     self.crawl_data['url'] = self.url
     self.crawl_data['id'] = mmh3.hash(self.url)
 
@@ -34,26 +36,41 @@ class Crawlr:
     return self.crawl_data['id']
 
   def crawl(self):
-    response = requests.get(self.url)
-    soup = bs4.BeautifulSoup(response.text)
     self.crawl_data['crawled_at'] = int(time.time())
-    self.crawl_data['title'] = soup.html.head.title.get_text() #.encode('ascii','ignore')
-    self.crawl_data['body'] = '\n'.join([''.join(s.findAll(text=True))for s in soup.findAll('p')]) #.encode('ascii','ignore')
-    self.crawl_data['internal_links'] = []
-    self.crawl_data['outbound_links'] = []
-    links = []
-    for a in soup.findAll('a'):
-      if a['href'].startswith('http'):
-        links.append(urlnorm.norm(a['href']))
 
-    # Remove dupe urls as we normalize them upon adding them to the list.
-    links = list(set(links))
+    headers = {'user-agent': 'crawlr/0.0.1'}
 
-    for link in links:
-      # The 3rd '/' in a normalized url seperates the domain from the path.
-      if self.tld in link.split('/')[2]:
-        self.crawl_data['internal_links'].append(link)
-      else:
-        self.crawl_data['outbound_links'].append(link)
+    try:
+      response = requests.get(self.url, headers=headers)
+    except requests.exceptions.Timeout:
+      self.crawl_data['failure'] = True
+      self.crawl_data['last_failure'] = 'Timeout'
+    except requests.exceptions.TooManyRedirects:
+      self.crawl_data['failure'] = True
+      self.crawl_data['last_failure'] = 'TooManyRedirects'
+    except requests.exceptions.RequestException as e:
+      self.crawl_data['failure'] = True
+      self.crawl_data['last_failure'] = 'timeout'
+
+    if self.crawl_data['failure'] == False: 
+      soup = bs4.BeautifulSoup(response.text)
+      self.crawl_data['title'] = soup.html.head.title.get_text() #.encode('ascii','ignore')
+      self.crawl_data['body'] = '\n'.join([''.join(s.findAll(text=True))for s in soup.findAll('p')]) #.encode('ascii','ignore')
+      self.crawl_data['internal_links'] = []
+      self.crawl_data['outbound_links'] = []
+      links = []
+      for a in soup.findAll('a'):
+        if a['href'].startswith('http'):
+          links.append(urlnorm.norm(a['href']))
+
+      # Remove dupe urls as we normalize them upon adding them to the list.
+      links = list(set(links))
+
+      for link in links:
+        # The 3rd '/' in a normalized url seperates the domain from the path.
+        if self.tld in link.split('/')[2]:
+          self.crawl_data['internal_links'].append(link)
+        else:
+          self.crawl_data['outbound_links'].append(link)
 
     return self.crawl_data
