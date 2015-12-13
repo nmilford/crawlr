20c8679d81aaa1277d91a20d0afff1f9cfa7f78e
Nathan Milford <nathan@milford.io>
2015-12-12 21:20:11 -0500

    now writes to cassandra

--- crawlr/__init__.py
+++ crawlr/__init__.py
@@ -1,3 +1,6 @@
+from cassandra.util import datetime_from_timestamp
+from cassandra.query import dict_factory
+from cassandra.cluster import Cluster
 from tld import get_tld
 import requests
 import urlnorm
@@ -6,20 +9,6 @@ import mmh3
 import bs4
 
 class Crawlr:
-  """
-  Crawls a given URL and returns a dict containing
-  {
-    'failures': integer of the number of failures,
-    'url_id': MurmerHash3 of the normalized URL to use as an ID,
-    'url': normalized URL,
-    'crawled_at': integer of the unixtime of the crawl,
-    'title': string of the URL's title,
-    'body': string of the URL's text,
-    'internal_links': list of links containing the original URL's TLD,
-    'external_links': list of links not containing the original URL's TLD
-  }
-  """
-
   def __init__(self, url):
     self.url = urlnorm.norm(url)
     self.tld = get_tld(url).encode('ascii','ignore')
@@ -27,17 +16,45 @@ class Crawlr:
     self.crawl_data = {}
     self.crawl_data['failure'] = False
     self.crawl_data['url'] = self.url
-    self.crawl_data['id'] = mmh3.hash128(self.url)
+    self.crawl_data['id'] = mmh3.hash64(self.url)[0]
 
-  def url(self):
-    return self.url
+    self.cassandra_cluster = ['127.0.0.1']
+    self.keyspace = 'crawlr'
 
-  def id(self):
-    return self.crawl_data['id']
+  def parse_links(self):
+    links = []
+    internal_links = []
+    outbound_links = []
 
-  def crawl(self):
-    self.crawl_data['crawled_at'] = int(time.time())
+    for a in self.soup.findAll('a'):
+      if a['href'].startswith('http'):
+        links.append(urlnorm.norm(a['href']))
 
+    # Remove dupe urls as we normalize them upon adding them to the list.
+    links = list(set(links))
+
+    for link in links:
+      # The 3rd '/' in a normalized url seperates the domain from the path.
+      if self.tld in link.split('/')[2]:
+        internal_links.append(link)
+      else:
+        outbound_links.append(link)
+
+    # I'm too lazy to fix this to get Cassandra to use a list. I can use .split(',') for now.
+    self.crawl_data['internal_links'] = ','.join(internal_links)
+    self.crawl_data['outbound_links'] = ','.join(outbound_links)
+
+    return self.crawl_data['internal_links'], self.crawl_data['outbound_links']
+
+  def parse_title(self):
+    self.crawl_data['title'] = self.soup.html.head.title.get_text()
+    return self.crawl_data['title']
+
+  def parse_body(self):
+    self.crawl_data['body'] = '\n'.join([''.join(s.findAll(text=True))for s in self.soup.findAll('p')])
+    return self.crawl_data['body']
+
+  def get_url(self):
     headers = {'user-agent': 'crawlr/0.0.1'}
 
     try:
@@ -56,27 +73,36 @@ class Crawlr:
       self.crawl_data['last_failure'] = 'TooManyRedirects'
     except requests.exceptions.RequestException as e:
       self.crawl_data['failure'] = True
-      self.crawl_data['last_failure'] = e 
+      self.crawl_data['last_failure'] = e
 
-    if self.crawl_data['failure'] == False: 
-      soup = bs4.BeautifulSoup(response.text)
-      self.crawl_data['title'] = soup.html.head.title.get_text() #.encode('ascii','ignore')
-      self.crawl_data['body'] = '\n'.join([''.join(s.findAll(text=True))for s in soup.findAll('p')]) #.encode('ascii','ignore')
-      self.crawl_data['internal_links'] = []
-      self.crawl_data['outbound_links'] = []
-      links = []
-      for a in soup.findAll('a'):
-        if a['href'].startswith('http'):
-          links.append(urlnorm.norm(a['href']))
+    if self.crawl_data['failure'] == False:
+      self.soup = bs4.BeautifulSoup(response.text)
 
-      # Remove dupe urls as we normalize them upon adding them to the list.
-      links = list(set(links))
-
-      for link in links:
-        # The 3rd '/' in a normalized url seperates the domain from the path.
-        if self.tld in link.split('/')[2]:
-          self.crawl_data['internal_links'].append(link)
-        else:
-          self.crawl_data['outbound_links'].append(link)
-
+  def crawl(self):
+    self.crawl_data['crawled_at'] = int(time.time())
+    self.get_url()
+    if self.crawl_data['failure'] == False:
+      self.parse_title()
+      self.parse_body()
+      self.parse_links()
     return self.crawl_data
+
+  def write_crawl_date(self):
+    cluster = Cluster(self.cassandra_cluster)
+    session = cluster.connect(self.keyspace)
+    session.row_factory = dict_factory
+
+    session.execute(
+      """
+      INSERT INTO pages (
+        id, url, crawled_at, failure, title, body, internal_links, outbound_links)
+      VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+      """,(self.crawl_data['id'],
+           self.crawl_data['url'],
+           datetime_from_timestamp(self.crawl_data['crawled_at']),
+           self.crawl_data['failure'],
+           self.crawl_data['title'],
+           self.crawl_data['body'],
+           self.crawl_data['internal_links'],
+           self.crawl_data['outbound_links'])
+     )
